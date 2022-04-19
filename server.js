98584f41f35e88cb14b7872687435ca6a4931922
acpaulsen <apaulsen43@gmail.com>
2022-04-18 23:44:53 -0400

    with function

--- server.js
+++ server.js
@@ -18,6 +18,30 @@ app.get('/app/', (req, res) => {
         res.end(res.statusCode+ ' ' +res.statusMessage)
 });
 
+//normal flip
+app.get('/app/flip/', (req, res) => {
+    res.status(200).json({"flip" : coinFlip()})
+});
+
+/** Simple coin flip
+ * 
+ * Write a function that accepts no parameters but returns either heads or tails at random.
+ * 
+ * @param {*}
+ * @returns {string} 
+ * 
+ * example: coinFlip()
+ * returns: heads
+ * 
+ */
+
+function coinFlip() {
+    let result = Math.random();
+    if (result <=.5)
+      return "tails"
+    return "heads"
+  }
+
 // Default response for any other request
 app.use(function(req, res){
     res.status(404).send('404 NOT FOUND')
